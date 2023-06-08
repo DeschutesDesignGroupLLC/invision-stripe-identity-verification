cc47c992c6a87e60efc892ba9e98a63378dc0b29
Jon Erickson <jon@deschutesdesigngroup.com>
2023-06-07 21:28:22 -0600

    Build 1.0.2

--- hooks/userBar.php
+++ hooks/userBar.php
@@ -11,34 +11,38 @@ if (! \defined('\IPS\SUITE_UNIQUE_KEY')) {
 class stripeverification_hook_userBar extends _HOOK_CLASS_
 {
     /* !Hook Data - DO NOT REMOVE */
-    public static function hookData()
-    {
-        return array_merge_recursive([
-            'userBar' => [
-                0 => [
-                    'selector' => '#elUserNav > li.cNotifications.cUserNav_icon',
-                    'type' => 'add_after',
-                    'content' => '<li class=\'cNotifications cUserNav_icon\'>
+public static function hookData() {
+ return array_merge_recursive( array (
+  'userBar' => 
+  array (
+    0 => 
+    array (
+      'selector' => '#elUserNav > li.cNotifications.cUserNav_icon',
+      'type' => 'add_after',
+      'content' => '<li class=\'cNotifications cUserNav_icon\'>
 			<a href=\'{url="app=stripeverification&module=system&controller=verification"}\' id=\'elVerificationStatus\' data-ipsTooltip title=\'{lang="stripeverification_userbar" escape="true"}\' data-ipsDialog data-ipsDialog-size="narrow" data-ipsDialog-title=\'{lang="stripeverification_userbar" escape="true"}\' >
 				<i class=\'fa fa-solid fa-circle-check\'></i>
             </a>
 		</li>',
-                ],
-                1 => [
-                    'selector' => '#elUserLink > i.fa.fa-caret-down',
-                    'type' => 'add_before',
-                    'content' => '{template="check" app="stripeverification" location="front" group="verification" params="\IPS\Member::loggedIn(), 16"}',
-                ],
-            ],
-            'userLink' => [
-                0 => [
-                    'selector' => 'a[rel=\'nofollow\'][data-ipshover-width=\'370\'].ipsType_break',
-                    'type' => 'add_after',
-                    'content' => '{template="check" app="stripeverification" location="front" group="verification" params="$member"}',
-                ],
-            ],
-        ], parent::hookData());
-    }
-    /* End Hook Data */
+    ),
+    1 => 
+    array (
+      'selector' => '#elUserLink > i.fa.fa-caret-down',
+      'type' => 'add_before',
+      'content' => '{template="check" app="stripeverification" location="front" group="verification" params="\IPS\Member::loggedIn(), 16"}',
+    ),
+  ),
+  'userLink' => 
+  array (
+    0 => 
+    array (
+      'selector' => 'a[data-ipshover]',
+      'type' => 'add_after',
+      'content' => '{template="check" app="stripeverification" location="front" group="verification" params="$member"}',
+    ),
+  ),
+), parent::hookData() );
+}
+/* End Hook Data */
 
 }
