9b39c68b8e84aa267b08fb8842836f35c346ee72
Jon Erickson <jon@deschutesdesigngroup.com>
2023-07-06 15:52:09 -0600

    Formatting

--- hooks/css.php
+++ hooks/css.php
@@ -1,9 +1,8 @@
 //<?php
 
 /* To prevent PHP errors (extending class does not exist) revealing path */
-if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
-{
-	exit;
+if (! \defined('\IPS\SUITE_UNIQUE_KEY')) {
+    exit;
 }
 
 /**
@@ -11,23 +10,20 @@ if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
  */
 class stripeverification_hook_css extends _HOOK_CLASS_
 {
-
-/* !Hook Data - DO NOT REMOVE */
-public static function hookData() {
- return array_merge_recursive( array (
-  'globalTemplate' => 
-  array (
-    0 => 
-    array (
-      'selector' => 'html > head',
-      'type' => 'add_inside_end',
-      'content' => '<link rel=\'stylesheet\' href=\'{expression="\IPS\Http\Url::external(reset(\IPS\Theme::i()->css(\'stripeverification.css\', \'stripeverification\', \'front\')))->setQueryString(\'v\',\IPS\Theme::i()->cssCacheBustKey())"}\' media=\'all\'>
+    /* !Hook Data - DO NOT REMOVE */
+    public static function hookData()
+    {
+        return array_merge_recursive([
+            'globalTemplate' => [
+                0 => [
+                    'selector' => 'html > head',
+                    'type' => 'add_inside_end',
+                    'content' => '<link rel=\'stylesheet\' href=\'{expression="\IPS\Http\Url::external(reset(\IPS\Theme::i()->css(\'stripeverification.css\', \'stripeverification\', \'front\')))->setQueryString(\'v\',\IPS\Theme::i()->cssCacheBustKey())"}\' media=\'all\'>
 ',
-    ),
-  ),
-), parent::hookData() );
-}
-/* End Hook Data */
-
+                ],
+            ],
+        ], parent::hookData());
+    }
+    /* End Hook Data */
 
 }
